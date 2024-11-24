2c9fa1289d9a83e4480e3ca5ae7523f619356573
Marcus Grenängen <marcus@grenangen.se>
2024-11-24 23:01:34 +0100

    Implement translations #19
     * Look for and apply any locale override requests on startup
     * Properly set and install translation files
     * Use compile defined path via cmake to know from where to load baked translation files
     * Fallback to using applicationDirPath() if failing to load from above defined path for translation files

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include <QStyleFactory>
 #endif
 
-int main(int argc, char *argv[])
+int main(int argc, char** argv)
 {
     QCoreApplication::setOrganizationName("grenangen");
     QCoreApplication::setOrganizationDomain("grenangen.se");
@@ -21,26 +21,52 @@ int main(int argc, char *argv[])
 #endif
     QApplication::setQuitOnLastWindowClosed(false);
 
+    // Locale setup
+    QLocale locale = QLocale::C;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "--locale") == 0) {
+            locale = QLocale(argv[++i]);
+        }
+    }
+
+    // No valid/or none given for locale at startup
+    if (locale == QLocale::C) {
+        locale = QLocale::system();
+    }
+    else {
+        // Set as default
+        QLocale::setDefault(locale);
+    }
+
+    qDebug() << "Will use locale: " << locale.name() << "for application";
+
+    // Setup and load translations
+    QString translationDirRoot = QApplication::applicationDirPath();
+#if defined(TRANSLATIONS_DIR)
+    qDebug() << "Using TRANSLATION_DIR from cmake compile definition!";
+    translationDirRoot = TRANSLATIONS_DIR;
+#endif
+
+    qDebug() << "Translation dir: " << translationDirRoot;
+
     QTranslator translator;
 
-    QLocale locale = QLocale::system();
-    QDir dir = QApplication::applicationDirPath();
-    auto didLoad = translator.load(locale, "tail_tray", "_", dir.absolutePath());
+    // Try to load
+    auto didLoad = translator.load(locale, "tail_tray", "_", translationDirRoot);
     if (didLoad)
         QCoreApplication::installTranslator(&translator);
     else {
-        // Load from default install din
-#if !defined(WINDOWS_BUILD)
-        // /usr/local/lib/tail-tray
-        dir = QDir("/usr/local/lib/tail-tray");
-        didLoad = translator.load(locale, "tail_tray", "_", dir.absolutePath());
+        // Load from fallback, eg same path as the binary is located at
+        didLoad = translator.load(locale, "tail_tray", "_",
+            QApplication::applicationDirPath());
+
         if (didLoad) {
             QCoreApplication::installTranslator(&translator);
         }
-#endif
+        else {
+            qDebug() << "Failed to load translations from all known locations";
+        }
     }
-
-    qDebug() << "Tail tray loaded translations from" << dir.absolutePath();
 
     MainWindow w;
     w.hide();

04139c18b893a0ec90115d5c795822f68f7e7f5c
Marcus Grenängen <marcus@grenangen.se>
2024-11-25 16:18:00 +0100

    Include translation files when making deb pkg

--- build-deb.sh
+++ build-deb.sh
@@ -5,13 +5,13 @@ if [ -z "$1" ]; then
     exit
 fi
 
-
 buildDir="$1"
 
 echo "Will use build directory $buildDir"
 
 # Create the directories 
 mkdir -p ./packaging/deb/tail-tray/usr/local/bin
+mkdir -p ./packaging/deb/tail-tray/usr/local/lib/tail-tray
 mkdir -p ./packaging/deb/tail-tray/usr/local/share/applications
 mkdir -p ./packaging/deb/tail-tray/usr/local/share/icons/hicolor/128x128/apps
 
@@ -19,12 +19,12 @@ mkdir -p ./packaging/deb/tail-tray/usr/local/share/icons/hicolor/128x128/apps
 cp "./icons/tailscale.png" ./packaging/deb/tail-tray/usr/local/share/icons/hicolor/128x128/apps
 cp "./tail-tray.desktop"   ./packaging/deb/tail-tray/usr/local/share/applications
 cp "$buildDir/tail-tray"   ./packaging/deb/tail-tray/usr/local/bin
+cp  $buildDir/*.qm          ./packaging/deb/tail-tray/usr/local/lib/tail-tray
 
 # Make sure the binary is executable
 chmod +x ./packaging/deb/tail-tray/usr/local/bin/tail-tray
 
 # And let's go to the deb pkg root and kick off the deb build
-
 cd ./packaging/deb
 
 # And build...

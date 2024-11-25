e8c443c4d2d1a4065525cbbbc936af866e818739
Marcus Grenängen <marcus@grenangen.se>
2024-11-25 13:01:21 +0100

    Implement translations #19
     * More Swedish translations

--- translations/tail_tray_sv_SE.ts
+++ translations/tail_tray_sv_SE.ts
@@ -32,27 +32,27 @@
         <location filename="src/MainWindow.ui" line="271"/>
         <source>tailnet_name</source>
         <translatorcomment>No translation needed</translatorcomment>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">tailnet_namn</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="305"/>
         <source>Email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Email</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="317"/>
         <source>email@email</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">email@email</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="351"/>
         <source>Status</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Status</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="363"/>
         <source>status_value</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">status_value</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="396"/>
@@ -62,75 +62,75 @@
     <message>
         <location filename="src/MainWindow.ui" line="409"/>
         <source>Admin Console</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Admin Panel</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="456"/>
         <source>Key expires</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nyckeln utgår</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="468"/>
         <source>Device key expires in ...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Enhetens nyckel utgår om...</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="498"/>
         <source>Reauthenticate...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Logga in igen...</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="546"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Inställningar</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="594"/>
         <source>General:</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Allmänt:</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="624"/>
         <source>Allow incoming connections</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tillåt inkommande anslutningar</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="636"/>
         <source>Use Tailscale DNS settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Använd Tailscale DNS inställningar</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="662"/>
         <location filename="src/MainWindow.ui" line="791"/>
         <location filename="src/MainWindow.ui" line="960"/>
         <source>Manage...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Hantera...</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="682"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Route traffic according to your network&apos;s rules. Some networks requires this to access IP addresses that don&apos;t start with 100.x.y.z.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Dirigera trafik enligt ditt nätverks regler. Vissa nätverk kräver detta för att komma åt IP-adresser som inte börjar med 100.x.y.z.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="685"/>
         <source>Use Tailscale subnets</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Använd Tailscale sub-nät</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="697"/>
         <source>Start on Login</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Starta vid inloggning</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="737"/>
         <source>VPN on demand</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">VPN på begäran</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="768"/>
         <location filename="src/MainWindow.ui" line="937"/>
         <source>Not enabled</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Inte aktiverad</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="829"/>
@@ -141,17 +141,17 @@
         <location filename="src/MainWindow.ui" line="848"/>
         <location filename="src/MainWindow.ui" line="1017"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Allows this machine to act as a gateway for your network. Other devices can then opt-in to routing all their traffic through it. &lt;span style=&quot; font-weight:700;&quot;&gt;Exit nodes&lt;/span&gt; must be &lt;span style=&quot; font-weight:700;&quot;&gt;approved in the admin console&lt;/span&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Tillåter den här maskinen att fungera som en gateway för ditt nätverk. Andra enheter kan sedan välja att dirigera all sin trafik genom den. &lt;span style=&quot; font-weight:700;&quot;&gt;Utgångsnoder&lt;/span&gt; måste vara&lt;span style=&quot; font-weight:700;&quot;&gt;godkända i administratörskonsolen&lt;/span&gt;.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="851"/>
         <source>Run as exit node</source>
-        <translation type="unfinished">Run as exit node</translation>
+        <translation type="unfinished">Kör som utgångsnod</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="877"/>
         <source>Allows direct access to the local network when routing traffic via an exit node.</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tillåter direkt åtkomst till det lokala nätverket vid dirigering av trafik via en utgångsnod.</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="880"/>
@@ -161,109 +161,109 @@
     <message>
         <location filename="src/MainWindow.ui" line="918"/>
         <source>Tailnet Lock</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tailnet lås</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="998"/>
         <location filename="src/MainWindow.ui" line="1120"/>
         <source>Tail Drive</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tail Drive</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1020"/>
         <source>Use Tail drive from this machine</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Använd Tail drive från denna maskin</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1058"/>
         <source>Default file save location for files shared from other devices</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Standardplats för fillagring för filer som delas från andra enheter</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1082"/>
         <location filename="src/MainWindow.ui" line="1266"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">...</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1123"/>
         <source>When tail drive is enabled, you can share folders from this machine over your tailnet</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">När tail drive är aktiverat kan du dela mappar från den här maskinen över ditt tailnet</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1132"/>
         <source>Active shares from this device</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Aktiva delningar från den här enheten</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1224"/>
         <source>Add</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Lägg till</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1231"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Ta bort</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1242"/>
         <source>Default mount path</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Standardväg för montering</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1298"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Important information&lt;br/&gt;&lt;/span&gt;On Linux, to be able to mount shares from other machines you will need to do some setup first:&lt;/p&gt;&lt;p&gt;Make sure to add http://100.100.100.100:8080 to your ~/.davfs2/secrets file&lt;/p&gt;&lt;p&gt;The file itself contains documentation on how to define this. For username you can enter Guest and leave password empty. Read more over at &lt;a href=&quot;https://wiki.archlinux.org/title/Davfs2&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;https://wiki.archlinux.org/title/Davfs2&lt;/span&gt;&lt;/a&gt; and at Tailscale &lt;a href=&quot;https://tailscale.com/kb/1369/taildrive?tab=linux&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;https://tailscale.com/kb/1369/taildrive?tab=linux&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Viktig information&lt;br/&gt;&lt;/span&gt;På Linux, för att kunna montera resurser från andra maskiner måste du göra några inställningar först:&lt;/p&gt;&lt;p&gt;Se till att lägga till http://100.100.100.100:8080 till din ~/.davfs2/secrets-fil&lt;/p&gt;&lt;p&gt;Själva filen innehåller dokumentation om hur man definierar detta. För användarnamn kan du ange Gäst och lämna lösenordet tomt. Läs mer på &lt;a href=&quot;https://wiki.archlinux.org/title/Davfs2&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;https://wiki.archlinux.org/title/Davfs2&lt;/span&gt;&lt;/a&gt; och hos Tailscale &lt;a href=&quot;https://tailscale.com/kb/1369/taildrive?tab=linux&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;https://tailscale.com/kb/1369/taildrive?tab=linux&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1329"/>
         <source>Fix it for me</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Fixa det åt mig</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1343"/>
         <source>Network Status</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Nätverksstatus</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1388"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Om</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1486"/>
         <source>Tailscale for Plasma Desktop</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Tailscale för Plasma Desktop</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1493"/>
         <source>Version 0.0.0.1</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Version 0.0.0.1</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1557"/>
         <source>WireGuard is a registered trademark of Jason A. Donenfeld</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">WireGuard är ett registrerat varumärke som tillhör Jason A. Donenfeld</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1609"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;© 2024 Tailscale Inc. All rights reserved. Tailscale is a registered trademark of Tailscale Inc.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;© 2024 Tailscale Inc. Med ensamrätt. Tailscale är ett registrerat varumärke som tillhör Tailscale Inc.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1661"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;This software is not affiliated with Tailscale in any way, shape or form&lt;/span&gt;. It&apos;s developed as an open source alternative with the intention of better integrating Tailscale with the Plasma desktop.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:700;&quot;&gt;Denna programvara är inte kopplad till Tailscale på något sätt&lt;/span&gt;. Det är utvecklat som ett alternativ med öppen källkod med avsikten att bättre integrera Tailscale med Plasma-skrivbordet.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1713"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Written and maintained by &lt;a href=&quot;mailto:marcus@grenangen.se&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;marcus@grenangen.se&lt;/span&gt;&lt;/a&gt;. Please visit the repo over at &lt;a href=&quot;https://github.com/SneWs/tail-tray&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;https://github.com/SneWs/tail-tray&lt;/span&gt;&lt;/a&gt; for more information&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Skriven och underhållen av &lt;a href=&quot;mailto:marcus@grenangen.se&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;marcus@grenangen.se&lt;/span&gt;&lt;/a&gt;. Information och kod finns här  &lt;a href=&quot;https://github.com/SneWs/tail-tray&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#7636dd;&quot;&gt;https://github.com/SneWs/tail-tray&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="src/MainWindow.ui" line="1776"/>
         <source>Close</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Stäng</translation>
     </message>
 </context>
 <context>
@@ -276,27 +276,27 @@
     <message>
         <location filename="src/ManageDriveWindow.ui" line="29"/>
         <source>Name</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Namn</translation>
     </message>
     <message>
         <location filename="src/ManageDriveWindow.ui" line="43"/>
         <source>Path</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Sökväg</translation>
     </message>
     <message>
         <location filename="src/ManageDriveWindow.ui" line="61"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">...</translation>
     </message>
     <message>
         <location filename="src/ManageDriveWindow.ui" line="100"/>
         <source>&amp;Ok</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Ok</translation>
     </message>
     <message>
         <location filename="src/ManageDriveWindow.ui" line="107"/>
         <source>&amp;Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">&amp;Avbryt</translation>
     </message>
 </context>
 <context>

e27e8e0390c36e6e5070aa4e81a640de2c1b47c5
Marcus Grenängen <marcus@grenangen.se>
2025-01-25 20:16:16 +0100

    Tailscale data parsing - Better value checking and handling

---
+++ src/models/JsonHelpers.h
@@ -0,0 +1,49 @@
+#ifndef JSONHELPERS_H
+#define JSONHELPERS_H
+
+#include <QString>
+#include <QJsonObject>
+
+namespace JsonHelpers {
+static QString safeReadStr(const QJsonObject& obj, const QString& key) {
+    if (!obj.contains(key))
+        return QString{};
+
+    if (obj[key].isNull())
+        return QString{};
+
+    return obj[key].toString("");
+}
+
+static int safeReadInt(const QJsonObject& obj, const QString& key) {
+    if (!obj.contains(key))
+        return 0;
+
+    if (obj[key].isNull())
+        return 0;
+
+    return obj[key].toInt();
+}
+
+static long long safeReadLong(const QJsonObject& obj, const QString& key) {
+    if (!obj.contains(key))
+        return 0;
+
+    if (obj[key].isNull())
+        return 0;
+
+    return obj[key].toInteger();
+}
+
+static bool safeReadBool(const QJsonObject& obj, const QString& key) {
+    if (!obj.contains(key))
+        return false;
+
+    if (obj[key].isNull())
+        return false;
+
+    return obj[key].toBool();
+}
+}
+
+#endif // JSONHELPERS_H
--- src/models/TailUser.h
+++ src/models/TailUser.h
@@ -22,16 +22,32 @@ public:
     static std::unique_ptr<TailUser> parse(const QJsonObject& obj, long long userId) {
         auto user = std::make_unique<TailUser>();
 
+        auto idKey = QString::number(userId);
+        if (!obj.contains(idKey) || obj[idKey].isNull()) {
+            return user;
+        }
+
         // Get the user object based on the user id (user id comes from the self part in the same doc)
-        auto thisUser = obj[QString::number(userId)].toObject();
+        auto thisUser = obj[idKey].toObject();
 
-        user->id = thisUser["ID"].toInteger(0L);
-        user->loginName = thisUser["LoginName"].toString("");
-        user->displayName = thisUser["DisplayName"].toString("");
-        user->profilePicUrl = thisUser["ProfilePicURL"].toString("");
+        user->id = userId;
 
-        for (const auto& ab : thisUser["roles"].toArray()) {
-            user->roles.emplace_back(ab.toString(""));
+        if (thisUser.contains("LoginName") && !thisUser["LoginName"].isNull())
+            user->loginName = thisUser["LoginName"].toString("");
+
+        if (thisUser.contains("DisplayName") && !thisUser["DisplayName"].isNull())
+            user->displayName = thisUser["DisplayName"].toString("");
+
+        if (thisUser.contains("ProfilePicURL") && !thisUser["ProfilePicURL"].isNull())
+            user->profilePicUrl = thisUser["ProfilePicURL"].toString("");
+
+        if (thisUser.contains("roles"))
+        {
+            for (const auto& ab : thisUser["roles"].toArray()) {
+                if (ab.isNull())
+                    continue;
+                user->roles.emplace_back(ab.toString(""));
+            }
         }
 
         return user;

6bb00ab87d47f118e50cd31f4e4b97398a209dd9
Marcus Grenängen <marcus@grenangen.se>
2025-03-19 14:08:16 +0100

    Naming, cleanup and a few notes

--- src/IpnWatcher.cpp
+++ src/IpnWatcher.cpp
@@ -2,7 +2,7 @@
 
 IpnWatcher::IpnWatcher(QObject *parent)
     : QObject(parent)
-    , m_pProcess(nullptr)
+    , m_process(nullptr)
     , m_isShuttingDown(false)
 {
 }
@@ -12,35 +12,35 @@ IpnWatcher::~IpnWatcher() {
 }
 
 void IpnWatcher::start() {
-    if (m_pProcess != nullptr) {
+    if (m_process != nullptr) {
         stop();
     }
 
     m_isShuttingDown = false;
 
-    m_pProcess = std::make_unique<QProcess>(this);
-    connect(m_pProcess.get(), &QProcess::finished,
+    m_process = std::make_unique<QProcess>(this);
+    connect(m_process.get(), &QProcess::finished,
         this, &IpnWatcher::onProcessFinished);
 
-    connect(m_pProcess.get(), &QProcess::readyReadStandardOutput,
+    connect(m_process.get(), &QProcess::readyReadStandardOutput,
         this, &IpnWatcher::onProcessCanReadStdOut);
 
-    connect(m_pProcess.get(), &QProcess::readAllStandardError,
+    connect(m_process.get(), &QProcess::readAllStandardError,
         this, &IpnWatcher::onProcessCanReadStandardError);
 
     QStringList args;
     args << "debug";
     args << "watch-ipn";
-    m_pProcess->start("tailscale", args);
+    m_process->start("tailscale", args);
 }
 
 void IpnWatcher::stop() {
     m_isShuttingDown = true;
-    if (m_pProcess->state() == QProcess::Running) {
-        m_pProcess->terminate();
+    if (m_process->state() == QProcess::Running) {
+        m_process->terminate();
     }
 
-    m_pProcess->reset();
+    m_process->reset();
 }
 
 void IpnWatcher::onProcessCanReadStdOut() {
--- src/IpnWatcher.h
+++ src/IpnWatcher.h
@@ -13,6 +13,11 @@ public:
     void stop();
 
 signals:
+    /// This will signal any time a new event comes in
+    /// TODO: We should scan for some specific well known event data types
+    /// so we can provide better feedback to listeners
+    /// but for now this will do just fine as the intended use case will be to re-read settings and check status
+    /// etc a-new every time this is emitted
     void eventReceived();
 
 private slots:
@@ -21,8 +26,8 @@ private slots:
     void onProcessFinished(int exitCode, QProcess::ExitStatus exitStatus);
 
 private:
-    std::unique_ptr<QProcess> m_pProcess;
+    std::unique_ptr<QProcess> m_process;
     bool m_isShuttingDown;
 };
 
-#endif //IPNWATCHER_H
+#endif // IPNWATCHER_H
